14b677123d8a3587b7a2f22038d6a2d08aa72498
eschmitty <30362930+eschmitty@users.noreply.github.com>
2018-06-13 02:24:18 -0400

    Various changes

--- cryptoKeys.sh
+++ cryptoKeys.sh
@@ -16,28 +16,28 @@ do
 	
 	###Pull prices from CoinMarketCap API and store in files
 	
-	(for i in $(cat /home/eschmitt/Daskeyboard5Q/coinlist | egrep -v '\#')
+	(for i in $(/home/eschmitt/Daskeyboard5Q/coinlist | grep -E -v '\#')
 	do
-	curl -s -X GET 'https://api.coinmarketcap.com/v2/ticker/'$i'/?structure=array' | grep change_24h | cut -d: -f2 | sed -e 's/ //g' -e 's/,//' > /home/eschmitt/Daskeyboard5Q/.$i; 
+	curl -s -X GET "https://api.coinmarketcap.com/v2/ticker/$i/?structure=array" | grep change_24h | cut -d: -f2 | sed -e 's/ //g' -e 's/,//' > /home/eschmitt/Daskeyboard5Q/."$i"; 
 	done)
 	
 	###Define 24h change value variables, create hundredth decimal place and remove decimal to create integer
 	
-	btc=$(cat /home/eschmitt/Daskeyboard5Q/.1 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	eth=$(cat /home/eschmitt/Daskeyboard5Q/.1027 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	ltc=$(cat /home/eschmitt/Daskeyboard5Q/.2 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	bch=$(cat /home/eschmitt/Daskeyboard5Q/.1831 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	xrp=$(cat /home/eschmitt/Daskeyboard5Q/.52 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	xlm=$(cat /home/eschmitt/Daskeyboard5Q/.512 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	ada=$(cat /home/eschmitt/Daskeyboard5Q/.2010 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	xmr=$(cat /home/eschmitt/Daskeyboard5Q/.328 | bc -l | xargs printf "%.2f" | sed 's/\.//')
-	burst=$(cat /home/eschmitt/Daskeyboard5Q/.573 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	btc=$(/home/eschmitt/Daskeyboard5Q/.1 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	eth=$(/home/eschmitt/Daskeyboard5Q/.1027 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	ltc=$(/home/eschmitt/Daskeyboard5Q/.2 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	bch=$(/home/eschmitt/Daskeyboard5Q/.1831 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	xrp=$(/home/eschmitt/Daskeyboard5Q/.52 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	xlm=$(/home/eschmitt/Daskeyboard5Q/.512 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	ada=$(/home/eschmitt/Daskeyboard5Q/.2010 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	xmr=$(/home/eschmitt/Daskeyboard5Q/.328 | bc -l | xargs printf "%.2f" | sed 's/\.//')
+	burst=$(/home/eschmitt/Daskeyboard5Q/.573 | bc -l | xargs printf "%.2f" | sed 's/\.//')
 	
 	###for each coin, send appropriate color code to API for current price change
 	
 	###BTC
 	###If BTC is 0 to 3.49%, send signal to set color green
-	if [ $btc -ge 0 ] && [ $btc -le 349 ]; then
+	if [ "$btc" -ge 0 ] && [ "$btc" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BTC +",
 		"pid": "DK5QPID",
@@ -49,7 +49,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BTC is +3.5 to 7.99%, send signal to blink color green
-	elif [ $btc -ge 350 ] && [ $btc -le 799 ]; then
+	elif [ "$btc" -ge 350 ] && [ "$btc" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BTC +",
 		"pid": "DK5QPID",
@@ -61,7 +61,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BTC is >+8%, send signal to color cycle
-	elif [ $btc -ge 800 ] && [ $btcstatus -ne 3 ]; then
+	elif [ "$btc" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BTC +",
 		"pid": "DK5QPID",
@@ -73,7 +73,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BTC is <0 to -3.49%, send signal to set color red
-	elif [ $btc -lt 0 ] && [ $btc -ge -349 ]; then
+	elif [ "$btc" -lt 0 ] && [ "$btc" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BTC -",
 		"pid": "DK5QPID",
@@ -85,7 +85,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BTC is <-3.5%, send signal to blink color red
-	elif [ $btc -le -350 ]; then
+	elif [ "$btc" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BTC -",
 		"pid": "DK5QPID",
@@ -101,7 +101,7 @@ do
 	
 	###ETH
 	###If ETH is 0 to 3.49%, send signal to set color green
-	if [ $eth -ge 0 ] && [ $eth -le 349 ]; then
+	if [ "$eth" -ge 0 ] && [ "$eth" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ETH +",
 		"pid": "DK5QPID",
@@ -113,7 +113,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ETH is +3.5 to 7.99%, send signal to blink color green
-	elif [ $eth -ge 350 ] && [ $eth -le 799 ]; then
+	elif [ "$eth" -ge 350 ] && [ "$eth" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ETH +",
 		"pid": "DK5QPID",
@@ -125,7 +125,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ETH is >+8%, send signal to color cycle
-	elif [ $eth -ge 800 ]; then
+	elif [ "$eth" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ETH +",
 		"pid": "DK5QPID",
@@ -137,7 +137,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ETH is <0 to -3.49%, send signal to set color red
-	elif [ $eth -lt 0 ] && [ $eth -ge -349 ]; then
+	elif [ "$eth" -lt 0 ] && [ "$eth" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ETH -",
 		"pid": "DK5QPID",
@@ -149,7 +149,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ETH is <-3.5%, send signal to blink color red
-	elif [ $eth -le -350 ]; then
+	elif [ "$eth" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ETH -",
 		"pid": "DK5QPID",
@@ -165,7 +165,7 @@ do
 	
 	###LTC
 	###If LTC is 0 to 3.49%, send signal to set color green
-	if [ $ltc -ge 0 ] && [ $ltc -le 349 ]; then
+	if [ "$ltc" -ge 0 ] && [ "$ltc" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "LTC +",
 		"pid": "DK5QPID",
@@ -177,7 +177,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If LTC is +3.5 to 7.99%, send signal to blink color green
-	elif [ $ltc -ge 350 ] && [ $ltc -le 799 ]; then
+	elif [ "$ltc" -ge 350 ] && [ "$ltc" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "LTC +",
 		"pid": "DK5QPID",
@@ -189,7 +189,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If LTC is >+8%, send signal to color cycle
-	elif [ $ltc -ge 800 ]; then
+	elif [ "$ltc" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "LTC +",
 		"pid": "DK5QPID",
@@ -201,7 +201,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If LTC is <0 to -3.49%, send signal to set color red
-	elif [ $ltc -lt 0 ] && [ $ltc -ge -349 ]; then
+	elif [ "$ltc" -lt 0 ] && [ "$ltc" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "LTC -",
 		"pid": "DK5QPID",
@@ -213,7 +213,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If LTC is <-3.5%, send signal to blink color red
-	elif [ $ltc -le -350 ]; then
+	elif [ "$ltc" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "LTC -",
 		"pid": "DK5QPID",
@@ -227,9 +227,9 @@ do
 	fi
 	printf "\n"
 	
-	###BCH
+	##BCH
 	###If BCH is 0 to 3.49%, send signal to set color green
-	if [ $bch -ge 0 ] && [ $bch -le 349 ]; then
+	if [ "$bch" -ge 0 ] && [ "$bch" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BCH +",
 		"pid": "DK5QPID",
@@ -241,7 +241,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BCH is +3.5 to 7.99%, send signal to blink color green
-	elif [ $bch -ge 350 ] && [ $bch -le 799 ]; then
+	elif [ "$bch" -ge 350 ] && [ "$bch" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BCH +",
 		"pid": "DK5QPID",
@@ -253,7 +253,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BCH is >+8%, send signal to color cycle
-	elif [ $bch -ge 800 ]; then
+	elif [ "$bch" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BCH +",
 		"pid": "DK5QPID",
@@ -265,7 +265,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BCH is <0 to -3.49%, send signal to set color red
-	elif [ $bch -lt 0 ] && [ $bch -ge -349 ]; then
+	elif [ "$bch" -lt 0 ] && [ "$bch" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BCH -",
 		"pid": "DK5QPID",
@@ -277,7 +277,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BCH is <-3.5%, send signal to blink color red
-	elif [ $bch -le -350 ]; then
+	elif [ "$bch" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BCH -",
 		"pid": "DK5QPID",
@@ -293,7 +293,7 @@ do
 	
 	###XRP
 	###If XRP is 0 to 3.49%, send signal to set color green
-	if [ $xrp -ge 0 ] && [ $xrp -le 349 ]; then
+	if [ "$xrp" -ge 0 ] && [ "$xrp" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XRP +",
 		"pid": "DK5QPID",
@@ -305,7 +305,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XRP is +3.5 to 7.99%, send signal to blink color green
-	elif [ $xrp -ge 350 ] && [ $xrp -le 799 ]; then
+	elif [ "$xrp" -ge 350 ] && [ "$xrp" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XRP +",
 		"pid": "DK5QPID",
@@ -317,7 +317,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XRP is >+8%, send signal to color cycle
-	elif [ $xrp -ge 800 ]; then
+	elif [ "$xrp" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XRP +",
 		"pid": "DK5QPID",
@@ -329,7 +329,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XRP is <0 to -3.49%, send signal to set color red
-	elif [ $xrp -lt 0 ] && [ $xrp -ge -349 ]; then
+	elif [ "$xrp" -lt 0 ] && [ "$xrp" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XRP -",
 		"pid": "DK5QPID",
@@ -341,7 +341,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XRP is <-3.5%, send signal to blink color red
-	elif [ $xrp -le -350 ]; then
+	elif [ "$xrp" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XRP -",
 		"pid": "DK5QPID",
@@ -357,7 +357,7 @@ do
 	
 	###XMR
 	###If XMR is 0 to 3.49%, send signal to set color green
-	if [ $xmr -ge 0 ] && [ $xmr -le 349 ]; then
+	if [ "$xmr" -ge 0 ] && [ "$xmr" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XMR +",
 		"pid": "DK5QPID",
@@ -369,7 +369,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XMR is +3.5 to 7.99%, send signal to blink color green
-	elif [ $xmr -ge 350 ] && [ $xmr -le 799 ]; then
+	elif [ "$xmr" -ge 350 ] && [ "$xmr" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XMR +",
 		"pid": "DK5QPID",
@@ -381,7 +381,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XMR is >+8%, send signal to color cycle
-	elif [ $xmr -ge 800 ]; then
+	elif [ "$xmr" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XMR +",
 		"pid": "DK5QPID",
@@ -393,7 +393,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XMR is <0 to -3.49%, send signal to set color red
-	elif [ $xmr -lt 0 ] && [ $xmr -ge -349 ]; then
+	elif [ "$xmr" -lt 0 ] && [ "$xmr" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XMR -",
 		"pid": "DK5QPID",
@@ -405,7 +405,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XMR is <-3.5%, send signal to blink color red
-	elif [ $xmr -le -350 ]; then
+	elif [ "$xmr" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XMR -",
 		"pid": "DK5QPID",
@@ -421,7 +421,7 @@ do
 	
 	###ADA
 	###If ADA is 0 to 3.49%, send signal to set color green
-	if [ $ada -ge 0 ] && [ $ada -le 349 ]; then
+	if [ "$ada" -ge 0 ] && [ "$ada" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ADA +",
 		"pid": "DK5QPID",
@@ -433,7 +433,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ADA is +3.5 to 7.99%, send signal to blink color green
-	elif [ $ada -ge 350 ] && [ $ada -le 799 ]; then
+	elif [ "$ada" -ge 350 ] && [ "$ada" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ADA +",
 		"pid": "DK5QPID",
@@ -445,7 +445,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ADA is >+8%, send signal to color cycle
-	elif [ $ada -ge 800 ]; then
+	elif [ "$ada" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ADA +",
 		"pid": "DK5QPID",
@@ -457,7 +457,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ADA is <0 to -3.49%, send signal to set color red
-	elif [ $ada -lt 0 ] && [ $ada -ge -349 ]; then
+	elif [ "$ada" -lt 0 ] && [ "$ada" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ADA -",
 		"pid": "DK5QPID",
@@ -469,7 +469,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If ADA is <-3.5%, send signal to blink color red
-	elif [ $ada -le -350 ]; then
+	elif [ "$ada" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "ADA -",
 		"pid": "DK5QPID",
@@ -485,7 +485,7 @@ do
 	
 	###XLM
 	###If XLM is 0 to 3.49%, send signal to set color green
-	if [ $xlm -ge 0 ] && [ $xlm -le 349 ]; then
+	if [ "$xlm" -ge 0 ] && [ "$xlm" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XLM +",
 		"pid": "DK5QPID",
@@ -497,7 +497,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XLM is +3.5 to 7.99%, send signal to blink color green
-	elif [ $xlm -ge 350 ] && [ $xlm -le 799 ]; then
+	elif [ "$xlm" -ge 350 ] && [ "$xlm" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XLM +",
 		"pid": "DK5QPID",
@@ -509,7 +509,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XLM is >+8%, send signal to color cycle
-	elif [ $xlm -ge 800 ]; then
+	elif [ "$xlm" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XLM +",
 		"pid": "DK5QPID",
@@ -521,7 +521,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XLM is <0 to -3.49%, send signal to set color red
-	elif [ $xlm -lt 0 ] && [ $xlm -ge -349 ]; then
+	elif [ "$xlm" -lt 0 ] && [ "$xlm" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XLM -",
 		"pid": "DK5QPID",
@@ -533,7 +533,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If XLM is <-3.5%, send signal to blink color red
-	elif [ $xlm -le -350 ]; then
+	elif [ "$xlm" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "XLM -",
 		"pid": "DK5QPID",
@@ -549,7 +549,7 @@ do
 	
 	###BURST
 	###If BURST is 0 to 3.49%, send signal to set color green
-	if [ $burst -ge 0 ] && [ $burst -le 349 ]; then
+	if [ "$burst" -ge 0 ] && [ "$burst" -le 349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BURST +",
 		"pid": "DK5QPID",
@@ -561,7 +561,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BURST is +3.5 to 7.99%, send signal to blink color green
-	elif [ $burst -ge 350 ] && [ $burst -le 799 ]; then
+	elif [ "$burst" -ge 350 ] && [ "$burst" -le 799 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BURST +",
 		"pid": "DK5QPID",
@@ -573,7 +573,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BURST is >+8%, send signal to color cycle
-	elif [ $burst -ge 800 ]; then
+	elif [ "$burst" -ge 800 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BURST +",
 		"pid": "DK5QPID",
@@ -585,7 +585,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BURST is <0 to -3.49%, send signal to set color red
-	elif [ $burst -lt 0 ] && [ $burst -ge -349 ]; then
+	elif [ "$burst" -lt 0 ] && [ "$burst" -ge -349 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BURST -",
 		"pid": "DK5QPID",
@@ -597,7 +597,7 @@ do
 		"isRead": true,
 		"isMuted": true}';
 	###If BURST is <-3.5%, send signal to blink color red
-	elif [ $burst -le -350 ]; then
+	elif [ "$burst" -le -350 ]; then
 	curl -s -H "Content-Type: application/json" -H "Authorization: Bearer $token" -X POST https://q.daskeyboard.com/api/1.0/signals -d '{
 		"name": "BURST -",
 		"pid": "DK5QPID",
